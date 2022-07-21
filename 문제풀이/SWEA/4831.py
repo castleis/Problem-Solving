397120f652a87c24d5f425c023493bcfcac12cc0
castleis <109266745+castleis@users.noreply.github.com>
2022-07-21 15:08:15 +0900

    아직 못풀음..

--- 문제풀이/SWEA/4831.py
+++ 문제풀이/SWEA/4831.py
@@ -1,17 +1,30 @@
 T = int(input())
 for t in range(1,T+1):
     k,n,m = input().split()
+    s = list(map(int, input().split()))
     k,n,m = int(k), int(n), int(m)
-    charge_idx = list(map(int,input().split()))
-    charge = [False]*(n+1)
 
-    for i in range(n+1):
-        if i in charge_idx:
-            charge[i] = True
-    bus = k
-    for j in range(n+1):
-        if 
+    station = [0]*(n+1)
+    station[0] = k
+    cnt = 0
 
+    for i in range(1,len(s)):
+        if s[i] - s[i-1] > k:
+            print(f'#{t}')
+            print(0)
+            continue
+
+    for i in range(1,n):
+        station[i] = station[i-1] -1
+        if i in s:
+            if n - i < k:
+                break
+            elif s[s.index(i)+2] - i > k:
+                cnt += 1
+                station[i] += k
+        else :
+            continue
+    print(station)
 
 
 
