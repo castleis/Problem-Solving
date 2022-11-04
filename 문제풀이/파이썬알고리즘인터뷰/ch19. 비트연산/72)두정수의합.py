65fec0165da70ae3b3f7899ab1a01b2f23495f53
castleis <tjddms7973@gmail.com>
2022-11-04 22:58:40 +0900

    1104

--- 문제풀이/파이썬알고리즘인터뷰/ch19. 비트연산/72)두정수의합.py
+++ 문제풀이/파이썬알고리즘인터뷰/ch19. 비트연산/72)두정수의합.py
@@ -2,8 +2,31 @@
 
 def getSum(a: int, b: int) -> int:
     MASK = 0xFFFFFFFF
-    bin_a = bin(a & MASK)[2:]
-    bin_b = bin(b & MASK)[2:]
+    bin_a = bin(a & MASK)[2:].zfill(32)
+    bin_b = bin(b & MASK)[2:].zfill(32)
+    num = ''
+    flag = False
+    for i in range(31,-1,-1):
+        if bin_a[i] and bin_b[i]:
+            if flag:
+                num = '1'+num
+            else:
+                num = '0'+num
+            flag = True
+            
+        elif bin_a[i] or bin_b[i]:
+            if flag:
+                num = '0'+num
+                flag = True
+            else:
+                num = '1'+num
+                flag = False
+        else:
+            if flag:
+                num = '1' + num
+            else:
+                num = '0' + num
+    print(int(num,2))
     
         
 
