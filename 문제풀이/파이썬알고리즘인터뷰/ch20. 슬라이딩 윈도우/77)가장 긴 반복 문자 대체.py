8f6067554ece792794cd96ea5ae7ad58fe8771dd
castleis <tjddms7973@gmail.com>
2022-11-11 00:01:35 +0900

    1110

--- 문제풀이/파이썬알고리즘인터뷰/ch20. 슬라이딩 윈도우/77)가장 긴 반복 문자 대체.py
+++ 문제풀이/파이썬알고리즘인터뷰/ch20. 슬라이딩 윈도우/77)가장 긴 반복 문자 대체.py
@@ -1,29 +1,35 @@
 # leetcode 424
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # 윈도우 크기는 1+k부터... 시작해도 될듯
-        i,j = 0,0
+        # k = 0인 경우, s가 모두 같은 문자인 경우, 마지막 문자 처리
         maxx = 0
-        cnt = k
-        for i in range(len(s)):
-            print(f'============== {i} ============')
-            for j in range(i,len(s)):
-                print(f'=== {j}, {cnt} ===')
-                if s[i] == s[j]:
-                    j += 1
-                else:
-                    if cnt == 0:
-                        print(f'끝  {j} => {j-i}')
-                        maxx = max(maxx, (j-i))
-                        cnt = k
-                        break
-                    cnt -= 1
-            if cnt == 0:
-                print(f'끝  {j} => {j-i+1}')
-                maxx = max(maxx, (j-i+1))
-                cnt = k
+        뇌절금지 = []       # 비교할 문자 필터링하는 용도
+        for start in range(len(s)):
+            i,j =  0,0
+            char = s[start]
+            if char in 뇌절금지:
+                continue
+            else:
+                뇌절금지.append(char)
+            while j < len(s):
+                j += 1
+                # print(f'====== {i}, {j} ======')
+                window = s[i:j]
+                # print(window.count(s[i]))
+                if (j - i - window.count(char)) <= k:
+                    # print('뀨')
+                    maxx = max(maxx, (j-i))
+                # 현재 윈도우 크기(j-i) 에서 기준 문자(s[i])의 갯수를 뺀 값이 k보다 크다면
+                # 어차피 다 바꿔도 안돼~~~~~~
+                # 그니까... i를 증가시켜주자
+                elif (j - i - window.count(char)) > k:
+                    i += 1
         return maxx
-s = "ABAB"
+
+# s = "ABAB"
+# s = "AAAB"
+s = "AAAA"
 k = 2
+k = 0
 a = Solution()
 print(a.characterReplacement(s,k))
