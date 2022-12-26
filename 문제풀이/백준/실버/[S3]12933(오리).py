e00d2dda0e7546e188076498adf7246ee0b8787d
castleis <tjddms7973@gmail.com>
2022-12-26 23:23:41 +0900

    백준시작

---
+++ 문제풀이/백준/실버/[S3]12933(오리).py
@@ -0,0 +1,33 @@
+def ducks(start):
+    global cnt
+    quack = 'quack'
+    j = 0
+    check = False
+    for i in range(start,length):
+        if duck[i] == quack[j] and not visited[i]:
+            visited[i] = 1
+            if duck[i] == 'k':
+                if not check:
+                    cnt += 1
+                    check = True
+                j = 0
+                continue
+            j += 1
+    return
+
+duck = input().strip()
+length = len(duck)
+print(length)
+visited = [0]*length
+cnt = 0
+if length % 5 != 0 or duck[0] != 'q':
+    print(-1)
+    quit()
+for i in range(length):
+    if duck[i] == 'q' and not visited[i]:
+        ducks(i)
+        # print(visited)
+if not cnt or not all(visited):
+    print(-1)
+else:
+    print(cnt)
