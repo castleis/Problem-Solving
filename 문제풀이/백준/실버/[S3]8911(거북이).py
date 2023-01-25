3e51f69105e12ec1a4f78c66f93d590685382b36
castleis <tjddms7973@gmail.com>
2023-01-25 22:36:51 +0900

    potato... :(

--- 문제풀이/백준/실버/[S3]8911(거북이).py
+++ 문제풀이/백준/실버/[S3]8911(거북이).py
@@ -1,7 +1,27 @@
+# 감자.. ㅜㅜ
+import sys
+input = sys.stdin.readline
+delta = [(0,1),(-1,0),(0,-1),(1,0)]
 for _ in range(int(input())):
     orders = list(input())
-    t_x, t_y = 0, 0
-    delta = [(1,0),(0,1),(-1,0),(0,-1)]
+    X,Y = 0,0
+    b_x, b_y, s_x, s_y = 0, 0, 0, 0
+    d = 0
     for order in orders:
         if order == 'F':
-            t_y
+            X += delta[d][0]
+            Y += delta[d][1]
+        elif order == 'B':
+            X += delta[(d+2)%4][0]
+            Y += delta[(d+2)%4][1]
+        elif order == 'L':
+            d = (d+1) % 4
+        elif order == 'R':
+            d = (d+3) % 4
+
+        b_x = max(X, b_x)
+        b_y = max(Y, b_y)
+        s_x = min(X, s_x)
+        s_y = min(Y, s_y)
+    print(abs(b_x - s_x) * abs(b_y - s_y))
+    
