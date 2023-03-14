da6ba66769d391e1af850331f66232e2533ead40
castleis <tjddms7973@gmail.com>
2023-03-14 17:49:52 +0900

    [BOJ] not yet.... :(

--- 문제풀이/백준/골드4/15685(드래곤커브).py
+++ 문제풀이/백준/골드4/15685(드래곤커브).py
@@ -2,20 +2,27 @@ N = int(input())
 info = [tuple(map(int,input().split())) for _ in range(N)]
 print(info)
 D = {0:(1,0),1:(0,-1),2:(-1,0),3:(0,1)}
-def rotate(center, pos):
-    X,Y = center
-    for n in range(len(pos)):
-        i,j = pos[n]
-        if i < X and j < Y:
-            i = 2*X-i
-            pos.append((i,j))
-        elif i > X and j < Y:
-            j = 2*Y-j
-            pos.append(i,j)
-        
+V = [[0]*101 for _ in range(101)]
+pos = []
+def rotate():
+    global pos
+    i,j,k = pos[-1]
+    for n in range(len(pos)-1):
+        x,y,d = pos[n]
+        dx,dy = D[d % 4]
+        pos.append((i+dx, j+dy, d%4))
+        i,j = i+dx, j+dy
+        V[i][j] = 1
     return
 for x,y,d,g in info:
-    pos = [(x,y)]
-    dx,dy = D[d]
-    center = (x+dx, y+dy)  # 0세대 좌표
-    rotate(center, pos)
+    pos.append((x,y,d))
+    V[x][y] = 1
+    for _ in range(g):
+        rotate()
+        print(pos)
+ans = 0
+for i in range(100):
+    for j in range(100):
+        if V[i][j] == V[i+1][j] == V[i][j+1] == V[i+1][j+1] == 1:
+            ans += 1
+print(ans)
